7ccb2bde0d45c030b552e8be03ef2131549e7f6c
Luojian Wei <luojianwei@pku.edu.cn>
2024-07-08 22:05:21 +0800

    config synctex

--- lisp/init-tex.el
+++ lisp/init-tex.el
@@ -18,12 +18,19 @@
   (add-to-list 'TeX-command-list '("LuaLaTeX" "%`lualatex%(mode)%' %t" TeX-run-TeX nil t))
   :hook
   (LaTeX-mode . turn-on-reftex)
+  (LaTeX-mode . TeX-source-correlate-mode)
   :config
   (setq TeX-auto-save t)
   (setq TeX-parse-self t)
   (setq-default TeX-master nil)
   (setq TeX-electric-sub-and-superscript t)
   (setq LaTeX-electric-left-right-brace t)
-  (setq reftex-plug-into-AUCTeX t))
+  (setq reftex-plug-into-AUCTeX t)
+  ;; Config SyncTeX
+  (setq TeX-source-correlate-method 'synctex)
+  (setq TeX-view-program-list
+	'(("PDF Viewer" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
+  (setq TeX-view-program-selection '((output-pdf "PDF Viewer"))))
 
+(server-start)
 (provide 'init-tex)
