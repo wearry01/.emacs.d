91b8073fc65386ab9a585cc3bbd80629ba492948
Luojian Wei <luojianwei@pku.edu.cn>
2025-06-06 12:45:51 +0800

    modify llm config

--- lisp/init-llm.el
+++ lisp/init-llm.el
@@ -14,12 +14,12 @@
   	  (make-llm-ollama
   	   ;; this model should be pulled to use it
   	   ;; value should be the same as you print in terminal during pull
-  	   :chat-model "qwen2.5"
+  	   :chat-model "phi4-mini"
   	   :embedding-model "nomic-embed-text"
-  	   :default-chat-non-standard-params '(("num_ctx" . 8192))))
+  	   :default-chat-non-standard-params '(("num_ctx" . 16384))))
   (setopt ellama-summarization-provider
   	  (make-llm-ollama
-  	   :chat-model "deepseek-r1"
+  	   :chat-model "qwen2.5"
   	   :embedding-model "nomic-embed-text"
   	   :default-chat-non-standard-params '(("num_ctx" . 32768))))
   (setopt ellama-coding-provider
@@ -44,12 +44,13 @@
   				      :chat-model "qwen2.5"
   				      :embedding-model "nomic-embed-text"
   				      :default-chat-non-standard-params '(("num_ctx" . 32768))))
+
+  ;; send last message in chat buffer with C-c C-c
+  ;; (add-hook 'org-ctrl-c-ctrl-c-hook #'ellama-chat-send-last-message)
+
   ;; customize display buffer behaviour
   ;; see ~(info "(elisp) Buffer Display Action Functions")~
   (setopt ellama-chat-display-action-function #'display-buffer-full-frame)
-  (setopt ellama-instant-display-action-function #'display-buffer-at-bottom)
-  ;; send last message in chat buffer with C-c C-c
-  ;; (add-hook 'org-ctrl-c-ctrl-c-hook #'ellama-chat-send-last-message)
-  )
+  (setopt ellama-instant-display-action-function #'display-buffer-at-bottom))
 
 (provide 'init-llm)
